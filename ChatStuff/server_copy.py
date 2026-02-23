4f54038fc8d1ed0c544fc1f6effe8f30c7ac7fce
apbadzioch <abadzioch@gmail.com>
2026-02-23 13:32:44 -0600

    work on server copy putting in better prompt

--- ChatStuff/server_copy.py
+++ ChatStuff/server_copy.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 from contextlib import asynccontextmanager
 
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, WebBaseLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader, WebBaseLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
@@ -22,19 +22,21 @@ class ChatRequest(BaseModel):
     message: str
 
 @asynccontextmanager
-async def lifespan(app:FastAPI):
+async def lifespan(app: FastAPI):
     print("starting system ...")
     app.state.llm = OllamaLLM(model="gemma3:1b", temperature=0.3)
     app.state.embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
     docs = []
-    docs += DirectoryLoader("info", glob="**/*.pdf", loader_cls=PyPDFLoader).load()
-    docs += DirectoryLoader('info')
+    docs += DirectoryLoader("../data", glob="**/*.pdf", loader_cls=PyPDFLoader).load()
+    docs += DirectoryLoader("../data", glob="**/*.txt", loader_cls=TextLoader).load()
 
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     chunks = splitter.split_documents(docs)
 
-    app.state.vectorstore = InMemoryVectorStore(chunks, app.state.embeddings)
+    vectorstore = InMemoryVectorStore(app.state.embeddings)
+    await vectorstore.aadd_documents(chunks)
+    app.state.vectorstore = vectorstore
 
     print("system loaded")
     yield
@@ -54,6 +56,15 @@ async def chat(request: ChatRequest):
     vs = app.state.vectorstore
 
     docs = vs.similarity_search(request.message, k=3)
-    response = llm.invoke(...)
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+                You are an assistant that answers questions from the given data.
+                Context: {context}
+                Question: {request.message}
+                Answer:
+                """
+
+    response = await llm.ainvoke(prompt)
 
     return {"reply": response}
